85d6fb5f03705b5e2d91a4f1094f092e55822a4e
Vairavan Srinivasan <vairav@codeaurora.org>
2024-09-19 22:08:47 -0700

    Handle tap to dismiss to invoke dismiss callback.

--- app/src/main/java/com/sample/composedialog/MainActivity.kt
+++ app/src/main/java/com/sample/composedialog/MainActivity.kt
@@ -3,23 +3,28 @@ package com.sample.composedialog
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalView
 import androidx.fragment.app.DialogFragment
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.fragment.app.FragmentManager
 import com.sample.composedialog.ui.theme.ComposeDialogTheme
-import kotlinx.coroutines.delay
 
 class MainActivity : FragmentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -39,33 +44,53 @@ class MainActivity : FragmentActivity() {
 
 @Composable
 fun Home(modifier: Modifier = Modifier) {
-    var showDialog by remember { mutableStateOf(false) }
+    var showDialog by rememberSaveable { mutableStateOf(false) }
 
-    LaunchedEffect(Unit) {
-        delay(1000)
-        showDialog = true
-        delay(5000)
-        showDialog = false
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier.fillMaxSize()
+    ) {
+        Button(onClick = { showDialog = true }) {
+            Text(text = "Show Dialog")
+        }
     }
 
     if (showDialog) {
-        DialogComposable<HelloDialogFragment>()
+        DialogComposable<HelloDialogFragment> {
+            showDialog = false
+        }
     }
 }
 
 @Composable
 inline fun <reified T : DialogFragment> DialogComposable(crossinline onDismiss: () -> Unit = {}) {
     val view = LocalView.current
+    var dismissNotified by remember { mutableStateOf(false) }
     val fragmentManager = remember(view) {
         FragmentManager.findFragmentManager(view)
     }
 
+    val fragmentLifecycleCallbacks = object : FragmentManager.FragmentLifecycleCallbacks() {
+        override fun onFragmentDestroyed(fm: FragmentManager, f: Fragment) {
+            if (f is T) {
+                onDismiss()
+                dismissNotified = true
+            }
+        }
+    }
+
     DisposableEffect(fragmentManager, T::class.java) {
         val dialogFragment = T::class.java.newInstance()
+        fragmentManager.registerFragmentLifecycleCallbacks(fragmentLifecycleCallbacks, true)
         dialogFragment.show(fragmentManager, "Dialog ${T::class.java.simpleName}")
+
         onDispose {
+            fragmentManager.unregisterFragmentLifecycleCallbacks(fragmentLifecycleCallbacks)
             dialogFragment.dismiss()
-            onDismiss()
+            if (!dismissNotified) {
+                onDismiss()
+            }
         }
     }
 }
